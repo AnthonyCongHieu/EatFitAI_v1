b6d90a984a842f6bb6b698cc42422d6c55d1d1b9
DinhCongHieu_FIVECAT_22CT113 <163360363+AnthonyCongHieu@users.noreply.github.com>
2025-10-19 10:06:59 +0700

    update BE

--- eatfitai-backend/src/EatFitAI.Domain/obj/Debug/net9.0/EatFitAI.Domain.AssemblyInfo.cs
+++ eatfitai-backend/src/EatFitAI.Domain/obj/Debug/net9.0/EatFitAI.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EatFitAI.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35f316896d6bf2f80da3ab46089f5aefc08aa635")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cee2fb38b1ad7aa92bcdba210709bc0ed50af19e")]
 [assembly: System.Reflection.AssemblyProductAttribute("EatFitAI.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EatFitAI.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
